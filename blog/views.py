068c94bd0a574bf0a274f83361a6460155d77ea1
bishal189 <bishal@gmail.com>
2023-08-02 20:43:03 +0545

    adding table for data stored

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,17 @@
 
 from django.shortcuts import redirect,render
+from bapp.models import Category ,Blog
 
 def home(request):
-    return render(request,'home/home.html')
+    category=Category.objects.all()
+    featured=Blog.objects.filter(is_featured=True,status='Published').order_by('created_at')
+    not_featured=Blog.objects.filter(is_featured=False,status='Published')
+    
+    context={
+        'category':category,
+        'featured':featured,
+        'not_featured':not_featured
+    }
+
+
+    return render(request,'home/home.html',context)
